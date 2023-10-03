e33a1f635dc05142d4f888c94d58ab5f88f19a74
Brian Alegre <brialegre@yahoo.com>
2023-10-02 22:27:42 -0700

    Updated _app.js to pass session as prop to children.  Update main index.js to have a signIn, signOut, and session

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,28 @@
-import { SessionProvider } from "next-auth/react"
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import React from 'react'
+import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
+export default function Home() {
+  const [session, loading] = useSession();
+
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <main className={styles.main}>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {session.user.email} <br />
+          <div> You have acceess to our pages</div>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+    </main>
+
   )
 }
