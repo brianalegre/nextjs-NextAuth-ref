ca10db6a850996a92a347da03bc0b15d4dc54a57
Brian Alegre <brialegre@yahoo.com>
2023-10-02 22:36:06 -0700

    Updated next-auth to use /react instead of /client, as /client is depreciated

--- src/pages/api/[...nextauth].js
+++ src/pages/api/[...nextauth].js
@@ -1,25 +1,41 @@
-// auth/[...nextauth].js
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+// // auth/[...nextauth].js
+// import NextAuth from 'next-auth'
+// import Providers from 'next-auth/providers'
 
-const options = {
+// const options = {
+//     providers: [
+//         Providers.Github({
+//             clientId: process.env.GITHUB_ID,
+//             clientSecret: process.env.GITHUB_SECRET,
+//         }),
+//         // Providers.Email({
+//         //     server: {
+//         //         host: '',
+//         //         port: '',
+//         //         auth: {
+//         //             user: '',
+//         //             pass: '',
+//         //         }
+//         //     },
+//         //     from: '',
+//         // }),
+//     ],
+// };
+
+// export default (req, res) => NextAuth(req, res, options);
+
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions = {
+    // Configure one or more authentication providers
     providers: [
-        Providers.Github({
+        GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
-        // Providers.Email({
-        //     server: {
-        //         host: '',
-        //         port: '',
-        //         auth: {
-        //             user: '',
-        //             pass: '',
-        //         }
-        //     },
-        //     from: '',
-        // }),
+        // ...add more providers here
     ],
-};
+}
 
-export default (req, res) => NextAuth(req, res, options);
+export default NextAuth(authOptions)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,15 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import React from 'react'
-import Link from 'next/link'
-import { signIn, signOut, useSession } from 'next-auth/client'
+import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Home() {
-  const [session, loading] = useSession();
-
-  return (
-    <main className={styles.main}>
-      {!session && (
-        <>
-          Not signed in <br />
-          <button onClick={() => signIn()}>Sign in</button>
-        </>
-      )}
-      {session && (
-        <>
-          Signed in as {session.user.email} <br />
-          <div> You have acceess to our pages</div>
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      )}
-    </main>
-
-  )
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return <>
+      Signed in as {session.user.email} <br />
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
+  return <>
+    Not signed in <br />
+    <button onClick={() => signIn()}>Sign in</button>
+  </>
 }
